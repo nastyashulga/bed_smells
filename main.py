ed96addcbabce07f63e40a06c2bd6eb2f029394b
Serafim <serafimabove@mail.ru>
2022-11-14 16:40:27 +0300

    the tests was add

--- main.py
+++ main.py
@@ -1,153 +1,36 @@
-from abc import ABC, abstractmethod
+from classes import Shop, Store, Request
+from storages_exceptions import BaseError
 
 
-class Storage(ABC):
-    def __init__(self):
-        self._items = {}
-        self._capacity = 0
+def main():
+    shop_1 = Shop(items={'картошка': 1, 'морковка': 2, 'лук': 3, 'помидор': 3, 'огурец': 2})
+    store_1 = Store(items={'диск': 1, 'кассета': 2, 'дискета': 3})
+    departments = {'магазин': shop_1, 'склад': store_1}
+    while True:
 
-    @abstractmethod
-    def add(self, name, amount):
-        pass
+        for item in departments:
+            print(f'В {item} хранится {departments[item].get_items()}')
 
-    @abstractmethod
-    def remove(self, name, amount):
-        pass
+        user_input = input('Доставить n товара из пункта А в пункт Б\nВведите "стоп" чтобы закончить\n').lower().strip()
 
-    @abstractmethod
-    def get_free_space(self):
-        pass
+        if user_input == 'стоп':
+            break
 
-    @abstractmethod
-    def get_items(self):
-        pass
+        try:
+            user_req = Request(user_input, departments)
 
-    @abstractmethod
-    def get_unique_items_count(self):
-        pass
+            destination = departments[user_req.destination]
+            departure = departments[user_req.departure]
 
+            departure.remove(user_req.product, user_req.amount)
+            destination.add(user_req.product, user_req.amount)
 
-
-
-
-class Store(Storage):
-    def __init__(self):
-        super().__init__()
-        self._capacity = 100
-
-    def add(self, name, amount):
-        if amount < self._capacity:
-            self._items[name] = self._items.get(name, 0) + amount
-            self._capacity -= amount
-
-        elif amount >= self._capacity:
-            difference = amount - self._capacity
-            self._items[name] = self._items.get(name, 0) + self._capacity
-            self._capacity = 0
-
-    def remove(self, name, amount):
-        if name not in self._items:
-            return 'Такого товара нет в магазине'
-
-        elif amount < self._items.get(name):
-            self._items[name] = self._items.get(name) - amount
-            self._capacity += amount
-
-        else:
-            difference = amount - self._items.get(name)
-            self._capacity += self._items.get(name)
-            del self._items[name]
-
-    def get_free_space(self):
-        return self._capacity
-
-    def get_items(self):
-        return self._items
-
-    def get_unique_items_count(self):
-        return len(self.get_items())
+            print(f'Курьер забрал {user_req.amount} {user_req.product} со {departure}')
+            print(f'Курьер везет {user_req.amount} {user_req.product} в {destination}')
+            print(f'Курьер доставил {user_req.amount} {user_req.product} в {destination}')
+        except BaseError as error:
+            print(error.message)
 
 
-class Shop(Storage):
-    def __init__(self):
-        super().__init__()
-        self._capacity = 20
-
-    def add(self, name, amount):
-        if self.get_unique_items_count() >= 5:
-            return 'В магазине уже 5 уникальных товаров'
-        else:
-            if amount < self._capacity:
-                self._items[name] = self._items.get(name, 0) + amount
-                self._capacity -= amount
-
-            elif amount >= self._capacity:
-                difference = amount - self._capacity
-                self._items[name] = self._items.get(name, 0) + self._capacity
-                self._capacity = 0
-
-    def remove(self, name, amount):
-        if name not in self._items:
-            return 'Такого товара нет в магазине'
-
-        elif amount < self._items.get(name):
-            self._items[name] = self._items.get(name) - amount
-            self._capacity += amount
-
-        else:
-            difference = amount - self._items.get(name)
-            self._capacity += self._items.get(name)
-            del self._items[name]
-
-    def get_free_space(self):
-        return self._capacity
-
-    def get_items(self):
-        return self._items
-
-    def get_unique_items_count(self):
-        return len(self._items)
-
-
-class Request:
-    pass
-
-
-store_1 = Shop()
-
-store_1.add(name="картошка", amount=1)
-print(store_1.get_items())
-print(store_1.get_free_space())
-print(store_1.get_unique_items_count())
-store_1.add(name="морковка", amount=1)
-print(store_1.get_items())
-print(store_1.get_free_space())
-print(store_1.get_unique_items_count())
-store_1.add(name="свекла", amount=1)
-print(store_1.get_items())
-print(store_1.get_free_space())
-print(store_1.get_unique_items_count())
-store_1.add(name="редька", amount=1)
-print(store_1.get_items())
-print(store_1.get_free_space())
-print(store_1.get_unique_items_count())
-store_1.add(name="лук", amount=1)
-print(store_1.get_items())
-print(store_1.get_free_space())
-print(store_1.get_unique_items_count())
-store_1.add(name="сельдерей", amount=1)
-print(store_1.get_items())
-print(store_1.get_free_space())
-print(store_1.get_unique_items_count())
-store_1.add(name="помидоры", amount=1)
-print(store_1.get_items())
-print(store_1.get_free_space())
-print(store_1.get_unique_items_count())
-store_1.add(name="огурцы", amount=1)
-print(store_1.get_items())
-print(store_1.get_free_space())
-
-print(store_1.get_unique_items_count())
-
-
-
+if __name__ == '__main__':
+    main()
---
+++ storages_exceptions.py
@@ -0,0 +1,27 @@
+class BaseError(Exception):
+    message = 'Неизвестная ошибка'
+
+
+class NotEnoughSpaceError(BaseError):
+    message = 'В выбранном пункте недостаточно места'
+
+
+class UnknownItemError(BaseError):
+    message = 'В выбранном пункте отсутствует данный товар'
+
+
+class NotEnoughItemError(BaseError):
+    message = 'В выбранном пункте недостаточно товара'
+
+
+class MoreThanFiveUniquesError(BaseError):
+    message = 'В выбранном пункте 5 или более уникальных товаров'
+
+
+class InvalidRequestError(BaseError):
+    message = 'Неправильный запрос'
+
+
+class UnknownStorageError(BaseError):
+    message = 'Неизвестный пункт'
+
